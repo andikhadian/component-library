3a9e2b932eb849beac89796db1eddc370bb8514a
Andikha Dian <andikha.nugraha@manuva.com>
2023-01-26 12:03:13 +0700

    Update Button API

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export const Button: React.FC = () => {
-  return <button>Button ay</button>;
-};
+type Props = React.ComponentPropsWithoutRef<'button'>;
+
+export const Button = React.forwardRef<HTMLButtonElement, Props>(
+  ({ children, ...props }, ref) => {
+    return (
+      <button ref={ref} type="button" {...props}>
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = 'Button';
